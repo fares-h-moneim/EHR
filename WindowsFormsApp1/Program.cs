0338c4cff4a8346e159c33f1896b4e127e0afcf3
YoussefDarwishh <youssefdarwishh@gmail.com>
2022-11-17 16:12:11 +0200

    darwish kan 3la el code w ms7 mody

--- WindowsFormsApp1/Program.cs
+++ WindowsFormsApp1/Program.cs
@@ -16,7 +16,8 @@ namespace WindowsFormsApp1
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1()); //mody on da code
+            Application.Run(new Form1());
+            //darwish on da code
         }
     }
 }
